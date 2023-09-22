6bf52ba904949e12fb9bdb796cd87b6709b6a9f8
alexst1910 <alexst1910@gmail.com>
2023-09-22 14:17:56 +0300

    added nav icons
    added nav icons

--- itp-library/src/components/Navigation/MainNavigation.tsx
+++ itp-library/src/components/Navigation/MainNavigation.tsx
@@ -2,35 +2,64 @@
 
 import { NavLink } from "react-router-dom";
 import classes from "../Navigation/MainNavigation.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faTruckFast,
+  faCartShopping,
+  faUser,
+  faBook,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+
 const MainNavigation = () => {
   return (
     <div className="container">
       <header>
         <nav className="navbar navbar-light justify-content-between">
-          <NavLink to="" className="navbar-brand">
+          <NavLink to="" className="navbar-brand d-flex align-items-center">
+            <FontAwesomeIcon icon={faBook} className={classes.logo} />
             <h2 className={classes.title}>ITP Library</h2>
           </NavLink>
+          <button
+            className="navbar-toggler ms-auto d-none d-sm-block border-0 d-lg-none"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarToggleExternalContent3"
+            aria-controls="navbarToggleExternalContent3"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <FontAwesomeIcon icon={faBars} className={classes.hamburger} />
+          </button>
 
-          <ul className="nav justify-content-end">
+          <ul className="nav justify-content-end d-none d-lg-inline-flex">
             <li className="nav-item">
               <NavLink to="" className="nav-link">
+                <FontAwesomeIcon icon={faHouse} className={classes.icon} />
                 <span className={classes.item}>HOME</span>
               </NavLink>
             </li>
 
             <li className="nav-item">
               <NavLink to="" className="nav-link">
+                <FontAwesomeIcon
+                  icon={faCartShopping}
+                  className={classes.icon}
+                />
                 <span className={classes.item}>SHOPPING CART</span>
               </NavLink>
             </li>
 
             <li className="nav-item">
               <NavLink to="" className="nav-link">
+                <FontAwesomeIcon icon={faTruckFast} className={classes.icon} />
                 <span className={classes.item}>ORDERS</span>
               </NavLink>
             </li>
             <li className="nav-item">
               <NavLink to="" className="nav-link">
+                <FontAwesomeIcon icon={faUser} className={classes.icon} />
                 <span className={classes.item}>LOGIN</span>
               </NavLink>
             </li>
