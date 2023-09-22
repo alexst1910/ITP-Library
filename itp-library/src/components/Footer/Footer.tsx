33ab7d72beed05e9e80f7cc6da376739c46522bd
alexst1910 <alexst1910@gmail.com>
2023-09-22 14:58:35 +0300

    added footer
    added footer

--- itp-library/src/components/Footer/Footer.tsx
+++ itp-library/src/components/Footer/Footer.tsx
@@ -1,8 +1,35 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classes from "../Footer/Footer.module.css";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
   let copyright = String.fromCodePoint(169);
+
+  const scroll = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    <div className="container text-light bg-secondary">
-      {copyright} Copyright IT Perspectives
+    <div
+      className={`container-fluid text-dark d-flex align-items-center ${classes.footer}`}
+    >
+      <div className="container d-flex justify-content-between">
+        <div className="pt-1">
+          {copyright} Copyright <b>IT Perspectives</b>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="btn btn-dark"
+            style={{ width: "40px" }}
+            onClick={scroll}
+          >
+            <FontAwesomeIcon icon={faArrowUp} style={{ color: "#ffffff" }} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
