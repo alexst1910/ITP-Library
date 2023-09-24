6a973b0cd15f2b56e5c0fc492973949740252113
alexst1910 <alexst1910@gmail.com>
2023-09-24 21:38:49 +0300

    update on navigation
    update on navigation

--- itp-library/src/components/Navigation/MainNavigation.tsx
+++ itp-library/src/components/Navigation/MainNavigation.tsx
@@ -1,7 +1,7 @@
 //import 'bootstrap/dist/css/bootstrap.css';
 
 import { NavLink } from "react-router-dom";
-import classes from "../Navigation/MainNavigation.module.css";
+import classNamees from "../Navigation/MainNavigation.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
@@ -18,9 +18,10 @@ const MainNavigation = () => {
       <header>
         <nav className="navbar navbar-light justify-content-between">
           <NavLink to="" className="navbar-brand d-flex align-items-center">
-            <FontAwesomeIcon icon={faBook} className={classes.logo} />
-            <h2 className={classes.title}>ITP Library</h2>
+            <FontAwesomeIcon icon={faBook} className={classNamees.logo} />
+            <h2 className={classNamees.title}>ITP Library</h2>
           </NavLink>
+
           <button
             className="navbar-toggler ms-auto d-none d-sm-block border-0 d-lg-none"
             type="button"
@@ -30,14 +31,14 @@ const MainNavigation = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <FontAwesomeIcon icon={faBars} className={classes.hamburger} />
+            <FontAwesomeIcon icon={faBars} className={classNamees.hamburger} />
           </button>
 
           <ul className="nav justify-content-end d-none d-lg-inline-flex">
             <li className="nav-item">
               <NavLink to="" className="nav-link">
-                <FontAwesomeIcon icon={faHouse} className={classes.icon} />
-                <span className={classes.item}>HOME</span>
+                <FontAwesomeIcon icon={faHouse} className={classNamees.icon} />
+                <span className={classNamees.item}>HOME</span>
               </NavLink>
             </li>
 
@@ -45,26 +46,40 @@ const MainNavigation = () => {
               <NavLink to="" className="nav-link">
                 <FontAwesomeIcon
                   icon={faCartShopping}
-                  className={classes.icon}
+                  className={classNamees.icon}
                 />
-                <span className={classes.item}>SHOPPING CART</span>
+                <span className={classNamees.item}>SHOPPING CART</span>
               </NavLink>
             </li>
 
             <li className="nav-item">
               <NavLink to="" className="nav-link">
-                <FontAwesomeIcon icon={faTruckFast} className={classes.icon} />
-                <span className={classes.item}>ORDERS</span>
+                <FontAwesomeIcon
+                  icon={faTruckFast}
+                  className={classNamees.icon}
+                />
+                <span className={classNamees.item}>ORDERS</span>
               </NavLink>
             </li>
             <li className="nav-item">
               <NavLink to="" className="nav-link">
-                <FontAwesomeIcon icon={faUser} className={classes.icon} />
-                <span className={classes.item}>LOGIN</span>
+                <FontAwesomeIcon icon={faUser} className={classNamees.icon} />
+                <span className={classNamees.item}>LOGIN</span>
               </NavLink>
             </li>
           </ul>
         </nav>
+        <div className="collapse" id="navbarToggleExternalContent3">
+          <div className="bg-light shadow-3 p-4">
+            <button className="btn btn-link btn-block border-bottom m-0">
+              Link 1
+            </button>
+            <button className="btn btn-link btn-block border-bottom m-0">
+              Link 2
+            </button>
+            <button className="btn btn-link btn-block m-0">Link 3</button>
+          </div>
+        </div>
       </header>
     </div>
   );
