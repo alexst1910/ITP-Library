4f90b0d470631e83e18064c04819e8f63b0e17e2
alexst1910 <alexst1910@gmail.com>
2023-10-09 22:17:47 +0300

    changes on firebase.ts
    changes on firebase.ts

--- itp-library/src/firebase.ts
+++ itp-library/src/firebase.ts
@@ -12,3 +12,23 @@ const app = firebase.initializeApp({
 
 export const auth = getAuth(app);
 export default app;
+
+/*<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAmYstQegGprHyNbCg68rlefnPWCUyDSQY",
+    authDomain: "itp-library-60637.firebaseapp.com",
+    projectId: "itp-library-60637",
+    storageBucket: "itp-library-60637.appspot.com",
+    messagingSenderId: "957278329811",
+    appId: "1:957278329811:web:728150b1e221f780f4ed26"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>*/
